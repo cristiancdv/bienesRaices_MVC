2013a450719dcccc1a28278c4e25f5784b00fe98
cristiancdv <cristiancdv@gmail.com>
2021-08-15 13:33:57 -0300

    commit on master

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,13 +1,14 @@
-<?php 
+<?php
 
-function conectarDB() {
-    $db = new mysqli('localhost', 'root', 'root', 'bienes-raices');
+function conectarDB()
+{
+    // $db = new mysqli('localhost', 'root', 'root', 'bienes-raices');
 
-    if(!$db) {
+    $db = new mysqli('ro2padgkirvcf55m.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'gt613zotzimgzrnb', 'f4825uruv6kqb5j3', 'v0cbex6a79dunokf');
+    if (!$db) {
         echo "Error no se pudo conectar";
         exit;
-    } 
+    }
 
     return $db;
-    
 }

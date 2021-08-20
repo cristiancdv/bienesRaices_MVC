fc16326df1c7caaf6fa6fbc9e954a0bc1c65bdfd
cristiancdv <cristiancdv@gmail.com>
2021-08-20 15:27:56 -0300

    commit on master

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 use MVC\Router;
 
-require_once __DIR__ . '/../includes/app.php';
+require_once __DIR__ . '/includes/app.php';
 require_once dirname(__FILE__) . "/Router.php";
 
 $routes = new Router(true);

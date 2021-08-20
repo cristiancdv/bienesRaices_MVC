c7a038665de905a43aeef34d74df6b850e9c8997
cristiancdv <cristiancdv@gmail.com>
2021-08-19 21:48:38 -0300

    commit on master

--- Router.php
+++ Router.php
@@ -4,17 +4,70 @@ namespace MVC;
 
 class Router
 {
+
+    private $basepath;
+    private $uri;
+    private $base_url;
+    private $routes;
+    private $route;
+    private $params;
+    private $get_params;
+
+    function __construct($get_params = false)
+    {
+        $this->get_params = $get_params;
+    }
+
+    public function getRoutes()
+    {
+        $this->base_url = $this->getCurrentUri();
+        $this->routes = explode('/', $this->base_url);
+
+        $this->getParams(); //invocamos el neuvo método
+        return $this->routes;
+    }
+
+    private function getCurrentUri()
+    {
+        $this->basepath = implode('/', array_slice(explode('/', $_SERVER['SCRIPT_NAME']), 0, -1)) . '/';
+        $this->uri = substr($_SERVER['REQUEST_URI'], strlen($this->basepath));
+
+        if ($this->get_params) {
+            $this->getParams();
+        } else {
+            if (strstr($this->uri, '?')) $this->uri = substr($this->uri, 0, strpos($this->uri, '?'));
+        }
+
+        $this->uri = '/' . trim($this->uri, '/');
+        return $this->uri;
+    }
+
+    private function getParams()
+    {
+        if (strstr($this->uri, '?')) {
+            $params = explode("?", $this->uri);
+            $params = $params[1];
+
+            parse_str($params, $this->params);
+            $this->routes[0] = $this->params;
+            array_pop($this->routes);
+        }
+    }
+
+
     public $rutasGET = [];
     public $rutasPOST = [];
 
-    public function get($url, $fn)
+    public function get($routes, $fn)
     {
-        $this->rutasGET[$url] = $fn;
+        $this->rutasGET[$routes] = $fn;
     }
-    public function post($url, $fn)
+    public function post($routes, $fn)
     {
-        $this->rutasPOST[$url] = $fn;
+        $this->rutasPOST[$routes] = $fn;
     }
+
+
 
 
     public function comprobarRutas()

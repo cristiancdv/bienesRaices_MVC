a6bab445029a4bf8e9e89de7bdc4d74239af3fa3
cristian <cristiancdv@gmail.com>
2021-12-20 20:09:07 -0300

    commit on master

--- includes/config/database.php
+++ includes/config/database.php
@@ -4,7 +4,7 @@ function conectarDB()
 {
     // $db = new mysqli('localhost', 'root', 'root', 'bienes-raices');
 
-    $db = new mysqli('ro2padgkirvcf55m.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'gt613zotzimgzrnb', 'f4825uruv6kqb5j3', 'v0cbex6a79dunokf');
+    $db = new mysqli('l6glqt8gsx37y4hs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'gkialma2i7re6zg3', 'n3qpkk48yzs34pjl', 'tpi2r3rzw1ahyldo', 3306);
     if (!$db) {
         echo "Error no se pudo conectar";
         exit;
